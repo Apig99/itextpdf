f0e0491d8b21891a3c31de9fa8f17a22de0e2088
Apig Aramian <apigaramian1@gmail.com>
2024-10-18 12:30:57 +0400

    first commit

---
+++ src/main/java/apig/aramian/itextpdfproject/ExportMetadata.java
@@ -0,0 +1,7 @@
+package apig.aramian.itextpdfproject;
+
+import java.util.HashMap;
+
+public class ExportMetadata extends HashMap<String, Object> {
+
+}
---
+++ src/main/java/apig/aramian/itextpdfproject/ItextPdfProjectApplication.java
@@ -0,0 +1,13 @@
+package apig.aramian.itextpdfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItextPdfProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItextPdfProjectApplication.class, args);
+    }
+
+}
